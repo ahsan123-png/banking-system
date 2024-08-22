919f5e8fc5243bbd1364f385d48a1c0aba2f64a1
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-08-22 20:23:55 +0500

    api interigation

--- bankingApp/frontend/urls.py
+++ bankingApp/frontend/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('register/', register,name="register"),
+    path('money/deposit', deposit_money_view,name="depositMoney"),
+    path('money/withdrawal', withdraw_money_view,name="withdrawMoney"),
+    path('online_transaction', onlineTransaction,name="onlineTransaction"),
     path('success/', success_view,name="success"),
 ]
--- bankingApp/frontend/views.py
+++ bankingApp/frontend/views.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import json
 from django.shortcuts import render
 from django.shortcuts import render, redirect
@@ -5,7 +6,11 @@ from .form import CreateUserForm, DepositMoneyForm, TransferFundsForm
 import requests
 from django.conf import settings
 from datetime import date
+
+API_URL='http://127.0.0.1:8000'
 #======== front end views =============
+
+#======== register New user ===============
 def register(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
@@ -24,40 +29,66 @@ def register(request):
             if response.status_code == 200:
                 return redirect('success')
             else:
-                # Print the error received from the API for debugging
-                print("API Error:", response.json())
                 return render(request, 'userCreate.html', {'form': form, 'error': response.json()})
         else:
             print("Form is invalid:", form.errors)  # Debug: Print form errors
     else:
         form = CreateUserForm()
     return render(request, 'userCreate.html', {'form': form})
+#============= Deposit money to bank account ================== 
+def deposit_money_view(request):
+    if request.method == 'POST':
+        form = DepositMoneyForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            for key, value in cleaned_data.items():
+                if isinstance(value, Decimal):
+                    cleaned_data[key] = str(value) 
+            response = requests.post(f'{API_URL}/users/deposit', data=json.dumps(cleaned_data),
+                headers={'Content-Type': 'application/json'})
+            if response.status_code == 200:
+                return redirect('success')
+            else:
+                return render(request, 'depositMony.html', {'form': form, 'error': response.json()})
+    else:
+        form = DepositMoneyForm()
+    return render(request, 'depositMony.html', {'form': form})
+#========== withdraw money from account ================
+def withdraw_money_view(request):
+    if request.method == 'POST':
+        form = DepositMoneyForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            for key, value in cleaned_data.items():
+                if isinstance(value, Decimal):
+                    cleaned_data[key] = str(value) 
+            response = requests.post(f'{API_URL}/users/withdraw', data=json.dumps(cleaned_data),
+                headers={'Content-Type': 'application/json'})
+            if response.status_code == 200:
+                return redirect('success')
+            else:
+                return render(request, 'withdraw.html', {'form': form, 'error': response.json()})
+    else:
+        form = DepositMoneyForm()
+    return render(request, 'withdraw.html', {'form': form})
 
-# def deposit_money_view(request):
-#     if request.method == 'POST':
-#         form = DepositMoneyForm(request.POST)
-#         if form.is_valid():
-#             response = requests.post(f'{API_URL}/deposit', data=form.cleaned_data)
-#             if response.status_code == 200:
-#                 return redirect('success')
-#             else:
-#                 return render(request, 'frontend/deposit_money.html', {'form': form, 'error': response.json()})
-#     else:
-#         form = DepositMoneyForm()
-#     return render(request, 'frontend/deposit_money.html', {'form': form})
-
-# def transfer_funds_view(request):
-#     if request.method == 'POST':
-#         form = TransferFundsForm(request.POST)
-#         if form.is_valid():
-#             response = requests.post(f'{API_URL}/transfer_funds', data=form.cleaned_data)
-#             if response.status_code == 200:
-#                 return redirect('success')
-#             else:
-#                 return render(request, 'frontend/transfer_funds.html', {'form': form, 'error': response.json()})
-#     else:
-#         form = TransferFundsForm()
-#     return render(request, 'frontend/transfer_funds.html', {'form': form})
+def onlineTransaction(request):
+    if request.method == 'POST':
+        form = TransferFundsForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            for key, value in cleaned_data.items():
+                if isinstance(value, Decimal):
+                    cleaned_data[key] = str(value) 
+            response = requests.post(f'{API_URL}/users/transfer_funds', data=json.dumps(cleaned_data),
+                headers={'Content-Type': 'application/json'})
+            if response.status_code == 200:
+                return redirect('success')
+            else:
+                return render(request, 'transferFunds.html', {'form': form, 'error': response.json()})
+    else:
+        form = TransferFundsForm()
+    return render(request, 'transferFunds.html', {'form': form})
 
 def success_view(request):
     return render(request, 'success.html')
